84e060f4d7f40d95f0542cfd65dc38c4604dae2c
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-05-17 11:11:36 +0000

    host and rediret uri - change form localhost to docker internal - chande redirect uri

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -14,7 +14,7 @@ class AuthBase:
             session.verify = current_app.config['REQUEST_SESSION']
         self.verify = os.environ.get('CURL_CA_BUNDLE','')
         
-        openID = Utils.fetch_url_data("https://localhost:9443/oauth2/token/.well-known/openid-configuration")
+        openID = Utils.fetch_url_data("https://host.docker.internal:9443/oauth2/token/.well-known/openid-configuration")
 
         self.signing_algos = openID["id_token_signing_alg_values_supported"]
         self.authorization_endpoint = openID['authorization_endpoint']
@@ -26,7 +26,7 @@ class AuthBase:
         self.my_account  = "https://localhost:9443/t/carbon.super/myaccount/overview"
 
 
-        self.redirect_uri = "https://localhost:5443/callback"
+        self.redirect_uri = "http://127.0.0.1:5000/callback"
         
         self.client = OAuth2Session(self.client_id, 
                        redirect_uri=self.redirect_uri, 
