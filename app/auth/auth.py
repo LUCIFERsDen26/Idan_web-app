6b9282563e250a080c177b710ec3fceb320bb47a
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-05-18 00:57:05 +0000

    cahnged from Authbase to Casdoor - Change WSO2 to casdoor

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,36 +1,39 @@
 # auth/auth.py
 
-from authlib.integrations.requests_client import OAuth2Session
+from casdoor import CasdoorSDK
 import requests
 import os 
 from .utils import Utils
 from flask import current_app   
 
-class AuthBase:
+class CasdoorAuthBase:
     def __init__(self):
+
         self.client_id = os.environ['CLIENT_ID']
+        self.client_certs = os.environ['CLIENT_CERTS']
+
         if current_app:
             session = requests.Session()
             session.verify = current_app.config['REQUEST_SESSION']
         self.verify = os.environ.get('CURL_CA_BUNDLE','')
-        
-        openID = Utils.fetch_url_data("https://host.docker.internal:9443/oauth2/token/.well-known/openid-configuration")
+                                       
+        openID = Utils.fetch_url_data("http://host.docker.internal:8000/.well-known/openid-configuration")
 
         self.signing_algos = openID["id_token_signing_alg_values_supported"]
         self.authorization_endpoint = openID['authorization_endpoint']
         self.token_endpoint = openID['token_endpoint']
-        self.revok_endpoint = openID['revocation_endpoint']
-        scope = 'openid email phone'
-        self.my_info_endpoint = openID['userinfo_endpoint']
         
-        self.my_account  = "https://localhost:9443/t/carbon.super/myaccount/overview"
-
+        self.scope = 'openid email phone'
+        self.my_info_endpoint = openID['userinfo_endpoint']
 
         self.redirect_uri = "http://127.0.0.1:5000/callback"
         
-        self.client = OAuth2Session(self.client_id, 
-                       redirect_uri=self.redirect_uri, 
-                       scope=scope, 
-                       code_challenge_method='S256'                       
+        self.CasdoorSdk = CasdoorSDK(
+                            endpoint=openID['issuer'],
+                            client_id=self.client_id,
+                            certificate=self.client_certs,
+                            org_name="IdanMotor",
+                            application_name="Idan_FrontEnd",                                                 
+                            client_secret=None                     
                       )
 
--- app/auth/login.py
+++ app/auth/login.py
@@ -1,10 +1,10 @@
 from flask_restful import Resource
-from .auth import AuthBase
+
 from flask import redirect, current_app
 from .utils import Utils
-class Login(Resource,AuthBase):
+class Login(Resource):
     def get(self):
         auth = current_app.auth_base
-        code_verifier = Utils.generate_code_verifier(48)
-        uri, state = auth.client.create_authorization_url(self.authorization_endpoint, code_verifier=code_verifier,redirect_uri=self.redirect_uri)
+        uri = auth.CasdoorSdk.get_auth_link(scope=auth.scope,redirect_uri=auth.redirect_uri)
+        print("\nURI : ",uri)
         return redirect(uri)
