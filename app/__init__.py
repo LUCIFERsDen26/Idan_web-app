956a213b9db518c51aadd401250be651f16f2ae1
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-05-19 15:46:02 +0530

    rest to monolitic: - converted to flask blueprint from flask-reslfull

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,22 @@
 # app/__init__.py
-#import config
-import os
-from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api
-from flask_swagger_ui import get_swaggerui_blueprint
 
-# import resources
-from .auth.auth import CasdoorAuthBase
-from .auth.login import Login
+import os
+from flask import Flask, session, current_app
+from flask_swagger_ui import get_swaggerui_blueprint
+# Import and register the auth blueprint
+from .auth import auth_bp, CasdoorAuthBase
 
 def create_app():
     app = Flask(__name__)
-    
-    environment_configuration = os.environ['CONFIGURATION_SETUP']    
-    app.config.from_object(environment_configuration)    
-    os.environ['FLASK_DEBUG'] =  app.config['DEBUG']   
+    app.secret_key = 'my_secret_key'
+
+    environment_configuration = os.environ['CONFIGURATION_SETUP']
+    app.config.from_object(environment_configuration)
+    os.environ['FLASK_DEBUG'] =  app.config['DEBUG']
 
     # Conditionally register Swagger UI if the environment is set to development
     if app.config['ENV'] == "development":
-        
+
         # Retrieve Swagger configuration from DevelopmentConfig
         swagger_url = app.config['SWAGGER_URL']
         api_url = app.config['API_URL']
@@ -32,15 +29,11 @@ def create_app():
         )
         app.register_blueprint(swagger_blueprint, url_prefix=swagger_url)
 
-    # Create the Flask-RESTful API instance
-    api = Api(app)
+    # Create an instance of the CasdoorAuthBase class and attach it to the application context
     with app.app_context():
-        # Initialize AuthBase and attach it to the application context
         app.auth_base = CasdoorAuthBase()
 
-    # add resources
-    api.add_resource(Login, '/auth/login')
-    
-
+    # Register the auth blueprint with the Flask app
+    app.register_blueprint(auth_bp)
 
     return app
