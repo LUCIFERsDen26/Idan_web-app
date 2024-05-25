5c97a1b6f2116149a2e6a0ac28a7bb99dfca8206
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-05-25 13:35:58 +0530

    added route: - added index route

--- app/index/resource/index.py
+++ app/index/resource/index.py
@@ -5,6 +5,6 @@ from flask import Blueprint, render_template
 index_bp = Blueprint('index_bp', __name__)
 
 @index_bp.route('/')
-def index():
+def index():    
     return render_template('index.html')
 
