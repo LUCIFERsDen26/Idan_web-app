6f1d4d98240604736b66fe498077e3ead5077972
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-05-25 13:34:35 +0530

    added session: - added to save code verifier and code challange

--- app/auth/resource/login.py
+++ app/auth/resource/login.py
@@ -1,5 +1,5 @@
 # auth/login.py
-from flask import Blueprint, redirect, request, session, current_app
+from flask import Blueprint, redirect, current_app
 from ..utils import Utils
 
 login_bp = Blueprint('login_bp', __name__)
@@ -8,6 +8,9 @@ login_bp = Blueprint('login_bp', __name__)
 def login():
     code_verifier, code_challenge = Utils.generate_code_verifier_and_code_challenge()
     uri = Utils.generate_auth_uri(code_challenge)
+    session = current_app.config['SESSION_REDIS']
+    
     session['code_verifier'] = code_verifier
+    session['code_challenge'] = code_challenge
     return redirect(uri)
 
