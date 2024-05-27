d4dc0e4876c204775d51194417782b34983abd4c
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-05-27 15:31:52 +0530

    checking wht is in session

--- app/routes/auth/resource/login.py
+++ app/routes/auth/resource/login.py
@@ -6,11 +6,16 @@ login_bp = Blueprint('login_bp', __name__)
 
 @login_bp.route('/login')
 def login():
+    session = current_app.config['SESSION_REDIS']
+    
     code_verifier, code_challenge = Utils.generate_code_verifier_and_code_challenge()
     uri = Utils.generate_auth_uri(code_challenge)
-    session = current_app.config['SESSION_REDIS']
+    
     
     session['code_verifier'] = code_verifier
     session['code_challenge'] = code_challenge
+
+    for key in session.keys():
+        print(f"key: {key} ,value: {session[key]}\n")
     return redirect(uri)
 
