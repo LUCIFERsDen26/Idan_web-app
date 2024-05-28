895bfd9b00549c4f9d58c6c2de2c60576f8ed924
LUCIFERsDen26 <bhushanrajendrashelar25@gmail.com>
2024-05-28 01:37:01 +0000

    added logging & check if user logged in.

--- app/routes/auth/resource/login.py
+++ app/routes/auth/resource/login.py
@@ -1,21 +1,25 @@
 # auth/login.py
-from flask import Blueprint, redirect, current_app
+from flask import Blueprint, redirect, current_app, flash
 from ..utils import Utils
-
+import logging
 login_bp = Blueprint('login_bp', __name__)
 
 @login_bp.route('/login')
 def login():
     session = current_app.config['SESSION_REDIS']
     
-    code_verifier, code_challenge = Utils.generate_code_verifier_and_code_challenge()
-    uri = Utils.generate_auth_uri(code_challenge)
+    if 'access_token' in session:
+        flash('you are already logged in')
+        logging.info('user has toekn in session')
+        return "<a href='/'>Home</a></br><p>you are already logged in</p>"
     
-    
-    session['code_verifier'] = code_verifier
-    session['code_challenge'] = code_challenge
-
-    for key in session.keys():
-        print(f"key: {key} ,value: {session[key]}\n")
-    return redirect(uri)
+    else:
+        logging.info('user has toekn not in session')
+        code_verifier, code_challenge = Utils.generate_code_verifier_and_code_challenge()
+        uri = Utils.generate_auth_uri(code_challenge)
+        
+        session['code_verifier'] = code_verifier
+        session['code_challenge'] = code_challenge
+        
+        return redirect(uri)
 
